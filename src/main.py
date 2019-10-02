628e250fa6968ddb6891cdeb472aea4ba176c826
jorgeuliana1 <ulianamjjorge@gmail.com>
2019-10-02 19:31:23 -0300

    Added support for multiple images generation

--- src/main.py
+++ src/main.py
@@ -44,7 +44,29 @@ def load_backgrounds(path, dimensions):
     # Can be used for ground textures and backgrounds
     return backgrounds
 
+def generate_outpath(folder_path, name_pattern, image_count, file_extension, max_number):
+
+    if not os.path.isdir(folder_path):
+        os.mkdir(folder_path)
+
+    if not os.path.isdir(folder_path+"/images"):
+        os.mkdir(folder_path+"/images")
+    
+    # Generating image number:
+    max_number_decimals = len(str(max_number))
+    count_str = list(str(image_count))
+    while len(count_str) < max_number_decimals:
+        count_str.insert(0, '0')
+    count_str = "".join(count_str)
+
+    # Returning the path:
+    return folder_path + "/images/" + name_pattern + count_str + file_extension
+
 def main():
+
+    # For information:
+    print("Preparing to generate dataset...")
+
     # Defining colors:
     RED   =  (  0,   0, 255)
     GREEN =  (  0, 255,   0)
@@ -74,7 +96,9 @@ def main():
     GROUND_TEXTURES = PATHS["GROUND_TEXTURES"]
     TEMPLATES = PATHS["TEMPLATES"]
     BACKGROUNDS = PATHS["BACKGROUND_IMAGES"]
-    DESTINY = PATHS["SAVE_PATH"]
+    DES_FOLDER = PATHS["DESTINY_FOLDER"]
+    FN_PATTERN = PATHS["FILENAME_PATTERN"]
+    F_EXTENSION = PATHS["FILENAME_EXTENSION"]
 
     POS = json_file["MARKPOS_VARIATION"]
     MINCX, MAXCX = POS["MIN_X"], POS["MAX_X"]
@@ -87,6 +111,9 @@ def main():
     MIN_XDIS, MAX_XDIS = SEP["MIN_XDIST"], SEP["MAX_XDIST"]
 
     SEED = json_file["SEED"]
+    NUMI = json_file["IMAGES"]
+
+    images_counter = 0 # To generate filename
 
     # Loading the roadmarks templates:
     templates = load_templates(TEMPLATES, (WIDTH, HEIGHT), ROAD_LANES)
@@ -95,66 +122,77 @@ def main():
     # Loading the backgrounds:
     backgrounds = load_backgrounds(BACKGROUNDS, (WIDTH, HEIGHT))
 
-    # Generating the templates layer:
-    overlay = generate_empty_templates_layer((WIDTH, HEIGHT))
+    for i in range(NUMI):
+        # Image generation loop
 
-    # Creating the image
-    img = RoadImage((WIDTH, HEIGHT), DESTINY, backgrounds, ground_textures, templates)
-    img.setSeed(SEED)
+        # Generating the templates layer:
+        overlay = generate_empty_templates_layer((WIDTH, HEIGHT))
 
-    # Defining the lanes in the road
-    img.defineLanes(ROAD_LANES, LANE_VARIATION)
+        # Creating the image
+        DESTINY = generate_outpath(DES_FOLDER, FN_PATTERN, images_counter, F_EXTENSION, NUMI)
+        img = RoadImage((WIDTH, HEIGHT), DESTINY, backgrounds, ground_textures, templates)
+        img.setSeed(SEED)
 
-    # Defining the separators:
-    separator_settings = img.getRandomSeparator(MINS_WID, MAXS_WID, MIND_SIZ, MAXD_SIZ,
-                                                MIND_SIZ, MAXD_SIZ, MIN_XDIS, MAX_XDIS)
+        # Defining the lanes in the road
+        img.defineLanes(ROAD_LANES, LANE_VARIATION)
 
-    road = img.getRoad()
-    # Inserting the lanes divisions in the road:
-    for i in range(1, ROAD_LANES):
-        if i > 0:
-            overlay = road.drawSeparatorByTuple(i - 1, overlay, separator_settings)
+        # Defining the separators:
+        separator_settings = img.getRandomSeparator(MINS_WID, MAXS_WID, MIND_SIZ, MAXD_SIZ,
+                                                    MIND_SIZ, MAXD_SIZ, MIN_XDIS, MAX_XDIS)
 
-    # Getting random background:
-    background_filename = img.randomBackground()
-    bg_img = backgrounds[background_filename]
+        road = img.getRoad()
+        # Inserting the lanes divisions in the road:
+        for i in range(1, ROAD_LANES):
+            if i > 0:
+                overlay = road.drawSeparatorByTuple(i - 1, overlay, separator_settings)
 
-    # Getting random ground texture:
-    ground_texture_filename = img.randomGround()
-    ground_texture = ground_textures[ground_texture_filename]
-    
-    # Inserting random template:
-    template_class = img.randomMark()
-    sample_template = templates[template_class]
-    lane_index = img.getRandomLane()
-    x_offset, y_offset = img.getShift(MINCX, MAXCX, MINCY, MAXCY)
-    overlay, template_location = road.insertTemplateAtLane(overlay,
-                                sample_template, lane_index, x=x_offset, y=y_offset)
+        # Getting random background:
+        background_filename = img.randomBackground()
+        bg_img = backgrounds[background_filename]
 
-    # Blending layers:
-    output_img = blend(ground_texture, overlay)
+        # Getting random ground texture:
+        ground_texture_filename = img.randomGround()
+        ground_texture = ground_textures[ground_texture_filename]
+        
+        # Inserting random template:
+        template_class = img.randomMark()
+        sample_template = templates[template_class]
+        lane_index = img.getRandomLane()
+        x_offset, y_offset = img.getShift(MINCX, MAXCX, MINCY, MAXCY)
+        overlay, template_location = road.insertTemplateAtLane(overlay,
+                                    sample_template, lane_index, x=x_offset, y=y_offset)
 
-    # Rotating image:
-    r_x, r_y, r_z = img.getRotation(MIN_X, MAX_X, MIN_Y, MAX_Y, MIN_Z, MAX_Z)
-    output_img = rotate(output_img, r_x, r_y, r_z, 0, 0, 0) # Must change later
-    output_img, y_shift = bring_to_bottom(output_img)
-    x_shift = 0
+        # Blending layers:
+        output_img = blend(ground_texture, overlay)
 
-    # Getting the template location
-    tracker = MarkTracker(DESTINY)
-    tracker.addLocation(template_location, template_class)
-    tracker.update(x_shift, y_shift, r_x)
+        # Rotating image:
+        r_x, r_y, r_z = img.getRotation(MIN_X, MAX_X, MIN_Y, MAX_Y, MIN_Z, MAX_Z)
+        output_img = rotate(output_img, r_x, r_y, r_z, 0, 0, 0) # Must change later
+        output_img, y_shift = bring_to_bottom(output_img)
+        x_shift = 0
 
-    # Final blending:
-    output_img = blend(bg_img, output_img)
+        # Getting the template location
+        tracker = MarkTracker(DESTINY)
+        tracker.addLocation(template_location, template_class)
+        tracker.update(x_shift, y_shift, r_x)
 
-    # Drawing bounding box around the template:
-    # draw_bbox(output_img, tracker.getLocation(0), RED) # Must change later
+        # Final blending:
+        output_img = blend(bg_img, output_img)
 
-    # TODO: FIX THE TRACKER
+        # Drawing bounding box around the template:
+        # draw_bbox(output_img, tracker.getLocation(0), RED) # Must change later
 
-    # Saving image:
-    save_image(output_img, path=DESTINY)
+        # TODO: FIX THE TRACKER
+
+        # Saving image:
+        save_image(output_img, path=DESTINY)
+        print("Generated image", images_counter+1, "of", NUMI)
+
+        # Preparing for the next round:
+        images_counter+=1
+        SEED+=1 # Must find a better solution later, but for now it works well
+
+    print("Dataset generated successfully.")
 
 if __name__ == "__main__":
     main()
