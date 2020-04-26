cf246a21cc4270520b219e30d987367d700a3a05
jorgeuliana1 <ulianamjjorge@gmail.com>
2020-04-26 19:33:26 -0300

    Fixed annotation filter error

--- src/extra_tools/filter_classes.py
+++ src/extra_tools/filter_classes.py
@@ -1,4 +1,4 @@
-import json, sys, os
+import json, sys, os, argparse
 
 def import_annotations(annotations_path):
     # Opening file
@@ -34,7 +34,7 @@ def import_classes(classes_path):
 
 def annotation_to_line(annotation):
     return "{},{},{},{},{},{}".format(annotation["path"],
-            annotation["x0"], annotation["x1"],
+            annotation["x0"], annotation["y0"],
             annotation["x1"], annotation["y1"],
             annotation["category"])
 
@@ -48,40 +48,22 @@ def keep_in_annotation(annotations, allowed_classes):
     return new_annotations
 
 def arguments():
-    args = sys.argv
-    
-    def get_tag_contents(args, tag):
 
-        for i in range(len(args)):
-            if args[i] == "--{}".format(tag):
-                break
-
-        def is_tag(string):
-            return "--" in string
-
-        j = i + 1
-        for j in range(i + 1, len(args)):
-            if is_tag(args[j]):
-                break
-
-        return tuple(args[i + 1:j])
-
-    output = get_tag_contents(args, "output")
-    classes = get_tag_contents(args, "classes")
-    annotations = get_tag_contents(args, "annotations")
-    keep = get_tag_contents(args, "keep")
-    substitute = get_tag_contents(args, "swap")
-
-    if len(substitute) % 2 != 0:
-        quit()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--output')
+    argparser.add_argument('--annotations')
+    argparser.add_argument('--keep')
+    argparser.add_argument('--swap', nargs='+')
+    argparser.add_argument('--classes')
+    values = vars(argparser.parse_args())
 
     return {
-            "output_folder" : output,
-            "classes_path" : classes,
-            "annotations_path": annotations,
-            "keep" : keep,
-            "substitute_from" : substitute[0],
-            "substitute_to" : substitute[1]
+            "output_folder" : values['output'],
+            "classes_path" : values['classes'],
+            "annotations_path": values['annotations'],
+            "keep" : values['keep'],
+            "substitute_from" : values['swap'][0],
+            "substitute_to" : values['swap'][1]
             }
 
 def dump(annotations, classes, output_dir):
@@ -98,12 +80,11 @@ def dump(annotations, classes, output_dir):
             if annotation != annotations[-1]: f.write("\n")
 
 args = arguments()
-annotations = import_annotations(args["annotations_path"][0])
-classes = import_classes(args["classes_path"][0])
+print(args)
+annotations = import_annotations(args["annotations_path"])
+classes = import_classes(args["classes_path"])
 
 substitute_in_annotation(annotations, args["substitute_from"], args["substitute_to"])
 annotations = keep_in_annotation(annotations, args["keep"])
 
-dump(annotations, classes, args["output_folder"][0])
-
-print(args)
+dump(annotations, classes, args["output_folder"])
